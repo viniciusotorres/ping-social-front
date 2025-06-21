424a06ef151ac7fcda31e5ddd508f09eab6e233c
viniciusotorres <viniciustorres29@gmail.com>
2025-06-21 17:52:52 -0300

    feat: Ajustes para dceploy.

--- src/app/modules/auth/services/register-service/register.service.ts
+++ src/app/modules/auth/services/register-service/register.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../../../../../environments/environments';
+import {environment} from '../../../../../environments/environment';
 
 export type RoleName = 'ROLE_USER' | 'ROLE_ADMIN';
 
