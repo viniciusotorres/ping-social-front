dc8547a9c1d157a4a846d13c11834c5511d4bb62
viniciusotorres <viniciustorres29@gmail.com>
2025-06-21 15:39:08 -0300

    feat: Ajustes e correção nos arquivos para deploy.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,15 @@ import {provideRouter, withComponentInputBinding, withHashLocation} from '@angul
 
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
       provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes, withComponentInputBinding(), withHashLocation()),
       provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+    }),
   ]
 };
